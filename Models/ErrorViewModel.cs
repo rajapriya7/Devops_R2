104971c4a442bf09d9fa97fbc89cbdd45834005b
rajapriya7 <rajapriyakalimuthu@gmail.com>
2022-08-12 12:33:00 +0530

    new commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Devops_R2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
